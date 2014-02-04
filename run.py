ba2105086d2d258509fb4c1f0185796a7ed3c31e
jwa <jack@andrewsmail.co.uk>
2014-02-04 12:06:16 +0000

    refactored

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
 import sys
 import re
 
+RUN_COMPETITION = False
+
+def load_hard_names():
+    with open('boy-names-hard.txt') as f:
+        names = f.read().split("\n")
+        return [x.strip() for x in names]
+
+def load_stdin_names():
+    names = sys.stdin.readlines()
+    names.pop(0)
+    return names
+
+#load and tokenise corpus into words of length > 1
 with open('corpus.txt') as f:
     corpus = f.read()
 
@@ -9,27 +22,19 @@ corpus = pattern.split(corpus.lower())
 corpus = [x.strip() for x in corpus]
 corpus = [x for x in corpus if len(x) > 1]
 
+#load lists of name to classify
+if RUN_COMPETITION:
+    names = load_stdin_names()
+else:
+    names = load_hard_names()
 
-
-#names = sys.stdin.readlines()
-#names.pop(0)
-#names = ['Hugh ']
-
-with open('boy-names-hard.txt') as f:
-    names = f.read().split("\n")
-names = [x.strip() for x in names]
-
-print len(names)
 
 for k, name in enumerate(names):
     name = name.strip().lower()
-    #print name
 
     indices = [i for i, x in enumerate(corpus) if x == name]
     if len(indices) == 0:
         continue
-
-    print indices
 
     male = ('he', 'his', 'him', 'himself', 'man', 'son', 'grandson', 'mr')
     female = ('she', 'hers', 'her', 'herself', 'woman', 'daughter', 'granddaughter', 'mrs')
@@ -37,14 +42,17 @@ for k, name in enumerate(names):
     sum_male = 0
     sum_female = 0
     for idx in indices:
-        print corpus[idx-10:idx+10]
         sum_male += sum([corpus[idx-10:idx+10].count(x) for x in male])
         sum_female += sum([corpus[idx-10:idx+10].count(x) for x in female])
 
-    print sum_male
-    print sum_female
-
-    if sum_male < sum_female:
-        #print 'Male'
-    #else:
+    if not RUN_COMPETITION:
         print str(k) + ' ' + name
+        print corpus[idx-10:idx+10]
+        print indices
+        print sum_male
+        print sum_female
+
+    if sum_male > sum_female:
+        print 'Male'
+    else:
+        print 'Female'
